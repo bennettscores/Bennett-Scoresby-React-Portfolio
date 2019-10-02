e0c43f78a5a11806e6b461c1f3d0ff88c25a01de
bennettscores <bennettscoresby1234@gmail.com>
2019-10-02 15:42:51 -0600

    added portfolio item

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import moment from 'moment';
 
-export default class App extends React.Component {
+import Portfolio from "./portfolio-container"
+
+export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Bennett Scoresby React Portfolio</h1>
+        <Portfolio />
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
